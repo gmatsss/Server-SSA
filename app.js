7dd0013d548210f01d4abe77772079a4bebfac91
Your Name~ <gmaturan60@gmail.com>
2023-10-03 13:17:16 +0800

    server update

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ app.use(
     resave: true,
     saveUninitialized: false,
     cookie: {
-      sameSite: "none", // or 'strict'
-      secure: true,
+      sameSite: "lax", // 'lax' is more lenient than 'strict' but still provides some level of CSRF protection
+      secure: false, // Set to false since you're not using HTTPS
       maxAge: 24 * 60 * 60 * 1000,
     },
   })
