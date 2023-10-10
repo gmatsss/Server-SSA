f0d6a222973238dfc09fbf81470a5b5ba27765ad
Your Name~ <gmaturan60@gmail.com>
2023-10-10 15:52:46 +0800

    update

--- controllers/retune.js
+++ controllers/retune.js
@@ -6,10 +6,10 @@ const API_URL = "https://retune.so";
 const apiProxy = proxy(API_URL, {
   proxyReqPathResolver: function (req) {
     if (req.url.startsWith("/create-thread")) {
-      return `/api/chat/11ee5547-2718-3180-8654-63b6aca533f6/new-thread`;
+      return `/api/chat/11ee5dba-b5d3-05f0-849b-e7dd1f1034f3/new-thread`;
     }
     if (req.url.startsWith("/get-messages")) {
-      return `/api/chat/11ee5547-2718-3180-8654-63b6aca533f6/messages`;
+      return `/api/chat/11ee5dba-b5d3-05f0-849b-e7dd1f1034f3/messages`;
     }
     return `/api${req.url}`;
   },
