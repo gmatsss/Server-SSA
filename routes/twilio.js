9e31fe4cec5dae51a51e7b32c4ae468611c41514
gmatsss <gmaturan60@gmail.com>
2024-07-17 14:55:16 +0800

    sad

--- routes/twilio.js
+++ routes/twilio.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { generateToken } = require("../controllers/twilio");
 
-router.get("/token", generateToken);
+router.post("/token", generateToken);
 // router.post("/call", generateToken);
 
 module.exports = router;
